d90da756f0f1589aa7edb2d829d318f7483d8073
louisvcarpet <blakeyuhao@gmail.com>
2026-03-30 17:20:57 -0400

    Edited Cuisuine handling for find_restuarants

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import pynyc as demo
 
 print("Welcome to the our NYC weekend planner service. Having trouble to decide what to do? We got you!")
 
-cuisine = input("Hi what you want for dinner? (American/Italian) ")
+cuisine = input("Hi what you want for dinner? (EXP: American/Italian/Chinese...etc.) ")
 restaurant = demo.find_restaurant(cuisine)
 
 print(f"Go to {restaurant}")
